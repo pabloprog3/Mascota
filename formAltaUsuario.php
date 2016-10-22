959f4b899a2517210cf97f31cf795bec808e3567
pabloprog3 <pablo_h_n@yahoo.com.ar>
2016-10-22 18:10:23 -0300

    Add files via upload

---
+++ formAltaUsuario.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/misEstilos.css">
+</head>
+<body class="Menu">
+
+	<h3>Registrar Usuario</h3>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+	<link rel="stylesheet" type="text/css" href="css/misEstilos.css">
+	
+	<script type="text/javascript" src="bootstrap-3.3.7-dist\js\bootstrap.min.js"></script>
+
+	<script type="text/javascript" src="inc\jquery-3.1.1.min.js"></script>
+
+	<script type="text/javascript" src="js\FuncionesParcial.js"></script>
+
+<!-- no funcionan asi
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist\css\bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist\css\bootstrap-theme.css"> -->
+
+	<title>Mascotas</title>
+</head>
+<body id="Menu" class="Menu">
+		<img id="foto" src="fotosSitio/animales/otro3.jpg" class="img-thumbnail img-circle">
+		<h1 id="Titulo"><u>ABM de Mascotas</u> <small id="subtitulo">con archivos de texto</small></h1>
+		<br>
+
+		<div class="container" id="Inicio">
+			<br><br>
+			 <button type="button" id="btnIngresar" class="btn btn-primary btn-block btnComun" onClick=> 
+			 	<span class="glyphicon glyphicon-play"></span>INGRESAR
+			 </button>
+
+			 <button type="button" id="btnReg" class="btn btn-primary btn-block btnComun" onclick="window.location.href='formAltaUsuario.php'"> 
+			 	<span class="glyphicon glyphicon-user"></span>REGISTRARSE
+			 </button>
+
+		</div>
+
+		<div  id="Respuesta">
+			
+		</div>
+
+
+</body>
+</html>
